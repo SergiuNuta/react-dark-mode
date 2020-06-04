7c1b595e2ac2aa2ae076a49ca5f9c48bcd34aec3
Serghei Nuta <sergiu.nuta@gmail.com>
2020-06-04 19:24:57 +0100

    working state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.scss";
+import sun from "./images/sun.png";
+import moon from "./images/moon.png";
 
 function App() {
+  const [darkMode, setDarkMode] = React.useState(getInitialMode());
+
+  React.useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  function getInitialMode() {
+    const isReturningUser = "dark" in localStorage;
+    const savedMode = JSON.parse(localStorage.getItem('dark'));
+    const userPrefersDark = getPreferColorScheme();
+    if (isReturningUser) {
+      return savedMode;
+    } else if (userPrefersDark) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function getPreferColorScheme() {
+    if (!window.matchMedia) return;
+
+    return window.matchMedia("(prefers-color-scheme: dark)");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <div className={darkMode ? styles.darkModeStyle : styles.lightModeStyle}>
+        <nav>
+          <div className={styles.toggleContainer}>
+            <span className={styles.toggle}>
+              <input
+                checked={darkMode}
+                onChange={() => setDarkMode(prevMode => !prevMode)}
+                type="checkbox"
+                className={styles.checkbox}
+                id={styles.checkbox} />
+              <label htmlFor="checkbox" />
+            </span>
+          </div>
+        </nav>
+        <div className={styles.card}>
+          <main>
+            <h1>{darkMode ? "Dark Mode" : "Light Mode"}</h1>
+            <img src={darkMode ? moon : sun} />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
